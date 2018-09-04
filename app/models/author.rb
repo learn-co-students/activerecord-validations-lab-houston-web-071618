f7ddefdb923fb5a48bdfefb4d865a6afc1eac390
github username <github email address>
2018-09-04 18:31:04 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    validates :name, {presence: true, uniqueness: true}
+
+    validates :phone_number, format: { with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/}
 end
